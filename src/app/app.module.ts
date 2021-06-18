1bf114bb4dc8e82770fea4f9c334647ae13a8b0e
Student2 <bal.serj@yandex.ru>
2021-06-18 14:19:24 +0300

    before routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card/card.component';
 import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FormComponent } from './form/form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/card/card/card.component.ts
+++ src/app/card/card/card.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
 
 title = 'MY CARD TITLE';
+text = 'My sample text';
+
+ngOnInit() {}
+
+changeTitle() {
+  this.title = 'Title has been changed!';
+}
+
+inputHealder(value) {
+  // const value = event.target.value;
+  this.title = value;
+}
+
+changeHealder() {
+  console.log(this.title)
+}
 
 }
